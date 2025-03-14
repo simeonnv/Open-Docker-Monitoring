64acbb9b3aaa7c86cc5a23dff34299a5a4f408be
Simeon <111970903+simeonnv@users.noreply.github.com>
2025-03-14 17:38:49 +0200

    guat

--- src/frontend/components/Dashboard/DashboardConnections/DashboardConnectionsCard.vue
+++ src/frontend/components/Dashboard/DashboardConnections/DashboardConnectionsCard.vue
@@ -10,6 +10,7 @@
     import type { DockerInfo } from '@/stores/docker';
     import { defineProps } from 'vue'; 
     import type { PropType } from 'vue'; 
+import Foldable from '@/components/ui/Foldable/Foldable.vue';
 
     const props = defineProps({
         docker: {
@@ -38,34 +39,40 @@
                 <span>{{ props.name }}</span>
                 <span>{{ docker.server_version || "unknown version!" }}</span> 
             </CardTitle>
-            <CardDescription><span>ID:</span> {{ docker.id }}</CardDescription>
+            <CardDescription>ID: {{ docker.id }}</CardDescription>
           </CardHeader>
 
-          <CardContent class="gap-2 flex flex-col">
 
-            <p>{{docker_connection.host}}</p>
+          <CardContent class="gap-2 flex flex-col">
+            <Foldable text="Show more information">
+
+                
+            <p>Protocol: {{docker_connection.protocol}}</p>
+
+            <p>Host address: {{docker_connection.host}}</p>
 
             <p>{{docker_connection.protocol}}</p>
 
-            <div v-if="docker.containers">
-                <p v-if="docker.containers" ><span>Contianers:</span> {{ docker.containers }}</p>
-                <p v-if="docker.containers_running" class="text-muted-foreground"> - <span>Containers Running:</span> {{ docker.containers_running }}</p>
-                <p v-if="docker.containers_paused" class="text-muted-foreground"> - <span>Containers Paused:</span> {{ docker.containers_paused }}</p>
-                <p v-if="docker.containers_stopped" class="text-muted-foreground"> - <span>Containers Stopped:</span> {{ docker.containers_stopped }}</p>
+            <div v-if="docker.containers !== undefined" class="flex flex-col">
+                <p v-if="docker.containers" >Contianers: {{ docker.containers }}</p>
+                <p v-if="docker.containers_running !== undefined" class="text-muted-foreground"> - Containers Running: {{ docker.containers_running }}</p>
+                <p v-if="docker.containers_paused !== undefined" class="text-muted-foreground"> - Containers Paused: {{ docker.containers_paused }}</p>
+                <p v-if="docker.containers_stopped !== undefined" class="text-muted-foreground"> - Containers Stopped: {{ docker.containers_stopped }}</p>
             </div>
 
-            <p v-if="docker.images" ><span>Images:</span> {{ docker.images }}</p>
+            <p v-if="docker.images" >Images: {{ docker.images }}</p>
             
-            <p v-if="docker.os_type" ><span>OS:</span> {{ docker.os_type }}</p>
+            <p v-if="docker.os_type" >OS: {{ docker.os_type }}</p>
             
-            <p v-if="docker.kernel_version" ><span>Kernel Version:</span> {{ docker.kernel_version }}</p>
+            <p v-if="docker.kernel_version" >Kernel Version: {{ docker.kernel_version }}</p>
 
-            <p v-if="docker.architecture" ><span>Architecture:</span> {{ docker.architecture }}</p>
+            <p v-if="docker.architecture" >Architecture: {{ docker.architecture }}</p>
 
-            <p v-if="docker.ncpu" ><span>CPU Cores:</span> {{ docker.ncpu }}</p>
+            <p v-if="docker.ncpu" >CPU Cores: {{ docker.ncpu }}</p>
 
-            <p v-if="docker.mem_total" ><span>Memory:</span> {{ Math.round(docker.mem_total / Math.pow(1024, 3) * 100) / 100 }}GB</p>
+            <p v-if="docker.mem_total" >Memory: {{ Math.round(docker.mem_total / Math.pow(1024, 3) * 100) / 100 }}GB</p>
 
+            </Foldable>
           </CardContent>
         </Card>
     </div>
---
+++ src/frontend/components/ui/Foldable/Foldable.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: ''
+  }
+})
+
+const folded = ref(true)
+import { useTimeout } from '@vueuse/core'
+
+const { ready, start } = useTimeout(300, { controls: true })
+
+
+
+
+</script>
+
+<template>
+
+    <div class="gap-2" v-auto-animate>
+        <Button @click="() => {if (ready) {folded = !folded; start()} }" variant="ghost" class="px-6 w-full flex-row flex p-0 m-0">
+            
+            <div class="flex flex-row w-full">
+                
+                <p>{{ text }}</p>
+                
+                <div class="grow" />
+                
+                <Icon 
+                name="material-symbols:arrow-back-2-rounded" 
+                    :class="folded ? 'rotate-90' : '-rotate-90'"
+                    class="h-6 w-6 transition-transform"
+                />
+                
+            </div>
+            
+        </Button>
+            
+        <div v-if="!folded">
+            <slot></slot>
+        </div>
+            
+    </div>
+
+</template>
